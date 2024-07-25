f6c915ceca05f1a5fceb91a755f2e8d514c187cf
eduairet <hola@eduairet.com>
2024-07-24 20:55:59 -0600

    Log error test fix

--- development_tools/tests/tests_log_messages.rs
+++ development_tools/tests/tests_log_messages.rs
@@ -1,19 +1,30 @@
 use development_tools::{log_debug_message, log_error_message};
-use log::error;
+use log::{error, LevelFilter};
+use std::sync::Once;
 
 #[cfg(test)]
 mod tests_log_messages {
     use super::*;
 
+    static INIT: Once = Once::new();
+
+    fn initialize() {
+        INIT.call_once(|| {
+            env_logger::builder()
+                .filter_level(LevelFilter::Debug)
+                .init();
+        });
+    }
+
     #[test]
     fn test_log_debug_message() {
-        env_logger::init();
+        initialize();
         log_debug_message("This is a debug message");
     }
 
     #[test]
     fn test_log_error_message() {
-        env_logger::init();
+        initialize();
         let result = log_error_message("This is an error message");
         assert_eq!(result, Err("ERROR: This is an error message".to_string()));
         if let Err(err) = result {
