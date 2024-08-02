ada5f6711a08f7e013f7f1c5b9b6a140601ff578
eduairet <hola@eduairet.com>
2024-08-01 19:14:01 -0600

    Encode a string as application/x-www-form-urlencoded

--- encoding/tests/tests_character_sets.rs
+++ encoding/tests/tests_character_sets.rs
@@ -1,4 +1,5 @@
 use encoding::{percent_decode_string, percent_encode_string};
+use url::form_urlencoded::{byte_serialize, parse};
 
 #[cfg(test)]
 mod tests_character_sets {
@@ -15,6 +16,22 @@ mod tests_character_sets {
 
         let decoded = percent_decode_string(&encoded).unwrap();
         print!("decoded: {}\n", decoded);
+        assert_eq!(decoded, input);
+    }
+
+    #[test]
+    fn test_encode_as_application_x_www_form_urlencoded() {
+        let input = "message=Hello, world!";
+        let expected = "message%3DHello%2C+world%21";
+
+        let encoded: String = byte_serialize(input.as_bytes()).collect();
+        print!("encoded: {}\n", encoded);
+        assert_eq!(encoded, expected);
+
+        let decoded: String = parse(encoded.as_bytes())
+            .map(|(key, val)| [key, val].concat())
+            .collect();
+        print!("decoded: {:?}\n", decoded);
         assert_eq!(decoded, input);
     }
 }
