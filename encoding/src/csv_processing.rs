da35a9e443a181ee35de59f1ec7e53f59dee8d56
eduairet <hola@eduairet.com>
2024-08-03 16:13:37 -0600

    CSV strong typed

--- encoding/src/csv_processing.rs
+++ encoding/src/csv_processing.rs
@@ -1,5 +1,27 @@
 use csv::Error;
+use serde::Deserialize;
 
+/// Read CSV records from a string.
+///
+/// # Arguments
+///
+/// * `csv_data` - A string slice that holds the CSV data.
+///
+/// # Returns
+///
+/// A vector of strings, where each string is a record from the CSV data.
+///
+/// # Example
+///
+/// ```
+/// use encoding::read_csv_records;
+///
+/// let csv_data = "name,age\nAlice,30\nBob,25\n";
+///
+/// let records = read_csv_records(csv_data).unwrap();
+///
+/// assert_eq!(records, vec!["Alice30", "Bob25"]);
+/// ```
 pub fn read_csv_records(csv_data: &str) -> Result<Vec<String>, Error> {
     let csv = csv_data;
 
@@ -17,4 +39,28 @@ pub fn read_csv_records(csv_data: &str) -> Result<Vec<String>, Error> {
     }
 
     Ok(records)
+}
+
+/// A token struct that represents a record in a CSV file.
+///
+/// # Example
+///
+/// ```
+/// use encoding::Token;
+///
+/// let token = Token {
+///    chain: "Ethereum".to_string(),
+///    name: "ETH".to_string(),
+///    ticker: "Ether".to_string(),
+///    price: 3000.0,
+/// };
+///
+/// assert_eq!(token.chain, "Ethereum");
+/// ```
+#[derive(Debug, Deserialize)]
+pub struct Token {
+    pub chain: String,
+    pub name: String,
+    pub ticker: String,
+    pub price: f64,
 }
--- encoding/tests/tests_csv_processing.rs
+++ encoding/tests/tests_csv_processing.rs
@@ -1,4 +1,4 @@
-use encoding::read_csv_records;
+use encoding::{read_csv_records, Token};
 
 #[cfg(test)]
 mod tests_csv_processing {
@@ -10,5 +10,31 @@ mod tests_csv_processing {
         let records = read_csv_records(csv_data).unwrap();
         print!("{:?}", records);
         assert_eq!(records, vec!["Alice30", "Bob25"]);
+    }
+
+    #[test]
+    fn test_read_csv_records_strong_type() {
+        let csv_data =
+            "chain,name,ticker,price\nEthereum,ETH,Ether,3000.0\nBitcoin,BTC,Bitcoin,60000.0\n";
+        let mut reader = csv::Reader::from_reader(csv_data.as_bytes());
+
+        let records = reader
+            .deserialize()
+            .collect::<Result<Vec<Token>, csv::Error>>()
+            .unwrap();
+
+        let ether = &records[0];
+        print!("{:?}", ether);
+        assert_eq!(ether.chain, "Ethereum");
+        assert_eq!(ether.name, "ETH");
+        assert_eq!(ether.ticker, "Ether");
+        assert_eq!(ether.price, 3000.0);
+
+        let bitcoin = &records[1];
+        print!("{:?}", bitcoin);
+        assert_eq!(bitcoin.chain, "Bitcoin");
+        assert_eq!(bitcoin.name, "BTC");
+        assert_eq!(bitcoin.ticker, "Bitcoin");
+        assert_eq!(bitcoin.price, 60000.0);
     }
 }
