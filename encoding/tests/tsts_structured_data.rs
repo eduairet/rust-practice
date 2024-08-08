5cbaf0f2c0ab7a0b758504ee746f74730e9c3462
eduairet <hola@eduairet.com>
2024-08-08 17:00:00 -0600

    Deserialize a TOML configuration file

--- encoding/tests/tsts_structured_data.rs
+++ encoding/tests/tsts_structured_data.rs
@@ -1,4 +1,5 @@
-use serde_json::{json, Value};
+use serde_json::{json, Value as JsonValue};
+use toml::Value as TomlValue;
 
 #[cfg(test)]
 mod tests_structured_data {
@@ -33,9 +34,28 @@ mod tests_structured_data {
             ]
         });
 
-        let parsed_data: Value = serde_json::from_str(json_data).unwrap();
+        let parsed_data: JsonValue = serde_json::from_str(json_data).unwrap();
 
         print!("Parsed data: {:?}", parsed_data);
         assert_eq!(parsed_data, expected);
+    }
+
+    #[test]
+    fn test_deserialize_toml() {
+        let toml_data = r#"
+            [package]
+            name = "encoding"
+            version = "0.1.0"
+            authors = ["Eduardo Aire <eat@beef.pnk>"]
+
+            [dependencies]
+            serde = "1.0"
+        "#;
+
+        let package_info: TomlValue = toml::from_str(toml_data).unwrap();
+
+        print!("Package info: {:?}", package_info);
+        assert_eq!(package_info["dependencies"]["serde"].as_str(), Some("1.0"));
+        assert_eq!(package_info["package"]["name"].as_str(), Some("encoding"));
     }
 }
