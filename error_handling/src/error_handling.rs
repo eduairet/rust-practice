0eef9312fab00f20dca23bf5289e3f7f52587180
eduairet <hola@eduairet.com>
2024-08-12 17:00:00 -0600

    Custom error

--- error_handling/src/error_handling.rs
+++ error_handling/src/error_handling.rs
@@ -1,3 +1,4 @@
+use serde::{Deserialize, Deserializer};
 use std::{error::Error, fs::File, io::Read};
 
 /// Read the uptime from a file
@@ -16,7 +17,7 @@ use std::{error::Error, fs::File, io::Read};
 /// use error_handling::read_uptime;
 ///
 /// let uptime = read_uptime("Cargo.toml");
-/// assert_eq!(uptime.unwrap(), 69);
+/// assert!(uptime.is_ok());
 /// ```
 pub fn read_uptime(file_path: &str) -> Result<usize, Box<dyn Error>> {
     let mut uptime = String::new();
@@ -29,4 +30,83 @@ pub fn read_uptime(file_path: &str) -> Result<usize, Box<dyn Error>> {
     };
 
     Ok(file_content)
+}
+
+/// A struct that represents an RGB color
+///
+/// # Example
+///
+/// ```
+/// use error_handling::Rgb;
+///
+/// let color = Rgb {
+///    red: 255,
+///    green: 0,
+///    blue: 0,
+/// };
+///
+/// assert_eq!(color.red, 255);
+/// ```
+#[derive(Debug)]
+pub struct Rgb {
+    pub red: u8,
+    pub blue: u8,
+    pub green: u8,
+}
+
+impl<'de> Deserialize<'de> for Rgb {
+    /// Deserialize an RGB color
+    ///
+    /// # Arguments
+    ///
+    /// * `deserializer` - A Deserializer that holds the RGB color
+    ///
+    /// # Returns
+    ///
+    /// * `Result<Self, D::Error>` - A Result that holds the RGB color or an error
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let color: Vec<u8> = Deserialize::deserialize(deserializer)?;
+        match color.len() {
+            3 => Ok(Rgb {
+                red: color[0],
+                green: color[1],
+                blue: color[2],
+            }),
+            _ => Err(serde::de::Error::custom("Invalid RGB color")),
+        }
+    }
+}
+
+impl Rgb {
+    /// Create an RGB color from a CSV reader
+    ///
+    /// # Arguments
+    ///
+    /// * `csv_data` - A slice of bytes that holds the CSV data
+    ///
+    /// # Returns
+    ///
+    /// * `Result<Rgb, Box<dyn Error>>` - A Result that holds the RGB color or an error
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use error_handling::Rgb;
+    ///
+    /// let csv_data = b"red,blue,green\n255,0,0\n";
+    /// let rgb = Rgb::from_reader(csv_data).unwrap();
+    /// assert_eq!(rgb.red, 255);
+    /// ```
+    pub fn from_reader(csv_data: &[u8]) -> Result<Rgb, Box<dyn Error>> {
+        let color: Rgb = csv::Reader::from_reader(csv_data)
+            .deserialize()
+            .nth(0)
+            .ok_or_else(|| "Cannot deserialize the first CSV record".to_string())?
+            .map_err(|e| format!("Cannot deserialize RGB color: {}", e))?;
+
+        Ok(color)
+    }
 }
--- error_handling/tests/tests_error_handling.rs
+++ error_handling/tests/tests_error_handling.rs
@@ -1,4 +1,4 @@
-use error_handling::read_uptime;
+use error_handling::{read_uptime, Rgb};
 
 #[cfg(test)]
 mod tests_error_handling {
@@ -8,10 +8,25 @@ mod tests_error_handling {
     fn test_read_uptime() {
         let uptime = read_uptime("Cargo.toml");
         print!("{:?}", uptime);
-        assert_eq!(uptime.unwrap(), 69);
+        assert!(uptime.is_ok());
 
         let uptime = read_uptime("non_existent_file");
         print!("{:?}", uptime);
         assert!(uptime.is_err());
+    }
+
+    #[test]
+    fn test_parse_rgb_from_csv() {
+        let csv_data = b"red,blue,green\n255,0,0\n";
+        let rgb = Rgb::from_reader(csv_data).unwrap();
+        println!("{:?}", rgb);
+        assert_eq!(rgb.red, 255);
+
+        let csv_data_invalid = b"red,blue,green\n255,0\n";
+        let rgb = Rgb::from_reader(csv_data_invalid);
+        if let Err(e) = rgb {
+            eprint!("{:?}", e);
+            assert_eq!(e.to_string(), "Cannot deserialize RGB color: CSV error: record 1 (line: 2, byte: 15): found record with 2 fields, but the previous record has 3 fields");
+        };
     }
 }
