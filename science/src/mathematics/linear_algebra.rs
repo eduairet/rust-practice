62b51b55f70185691e62bd070017d29cf0438cdb
eduairet <hola@eduairet.com>
2024-08-27 19:52:41 -0600

    Multiply matrices

--- science/src/mathematics/linear_algebra.rs
+++ science/src/mathematics/linear_algebra.rs
@@ -24,3 +24,31 @@ use ndarray::Array2;
 pub fn adding_2d_usize_matrices(a: &Array2<usize>, b: &Array2<usize>) -> ndarray::Array2<usize> {
     a + b
 }
+
+/// Multiplies two 2D matrices of `usize` values.
+///
+/// # Arguments
+///
+/// * `a` - A 2D matrix of `usize` values.
+/// * `b` - A 2D matrix of `usize` values.
+///
+/// # Returns
+///
+/// A 2D matrix of `usize` values.
+///
+/// # Example
+///
+/// ```
+/// use ndarray::arr2;
+///
+/// let a = arr2(&[[1, 2], [3, 4]]);
+/// let b = arr2(&[[5, 6], [7, 8]]);
+/// let c = arr2(&[[19, 22], [43, 50]]);
+/// assert_eq!(c, science::multiplying_2d_usize_matrices(&a, &b));
+/// ```
+pub fn multiplying_2d_usize_matrices(
+    a: &Array2<usize>,
+    b: &Array2<usize>,
+) -> ndarray::Array2<usize> {
+    a.dot(b)
+}
--- science/tests/tests_linear_algebra.rs
+++ science/tests/tests_linear_algebra.rs
@@ -1,5 +1,5 @@
 use ndarray::arr2;
-use science::adding_2d_usize_matrices;
+use science::{adding_2d_usize_matrices, multiplying_2d_usize_matrices};
 
 #[cfg(test)]
 mod tests_linear_algebra {
@@ -15,5 +15,16 @@ mod tests_linear_algebra {
         print!("b: {:?}\n", b);
         print!("c: {:?}\n", c);
         assert_eq!(c, adding_2d_usize_matrices(&a, &b));
+    }
+
+    #[test]
+    fn test_multiplying_2d_usize_matrices() {
+        let a = arr2(&[[1, 2], [3, 4]]);
+        let b = arr2(&[[5, 6], [7, 8]]);
+        let c = arr2(&[[19, 22], [43, 50]]);
+        print!("a: {:?}\n", a);
+        print!("b: {:?}\n", b);
+        print!("c: {:?}\n", c);
+        assert_eq!(c, multiplying_2d_usize_matrices(&a, &b));
     }
 }
