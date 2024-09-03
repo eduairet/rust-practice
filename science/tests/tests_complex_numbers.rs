fc4a0f89f7497b3e7507050918735654ca3bc03c
eduairet <hola@eduairet.com>
2024-09-02 23:23:37 -0600

    Mathematical functions

--- science/tests/tests_complex_numbers.rs
+++ science/tests/tests_complex_numbers.rs
@@ -1,5 +1,6 @@
 use num::complex::Complex;
 use science::add_complex_numbers;
+use std::f64::consts::PI;
 
 #[cfg(test)]
 mod tests_complex_numbers {
@@ -12,5 +13,12 @@ mod tests_complex_numbers {
         let sum = add_complex_numbers(nums);
         println!("sum: {}", sum);
         assert_eq!(sum, Complex::new(13.1, 15.8));
+    }
+
+    #[test]
+    fn test_complex_numbers_math() {
+        let x = Complex::new(0.0, 2.0 * PI);
+        println!("e^(2i * pi) = {}", x.exp());
+        assert_eq!(x.exp(), Complex::new(1.0, -2.4492935982947064e-16));
     }
 }
