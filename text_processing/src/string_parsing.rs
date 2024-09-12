113a9b93cbeae0f911289d5a94f4caaea1d71b70
eduairet <hola@eduairet.com>
2024-09-11 20:58:45 -0600

    Implement the FromStr trait for a custom struct

--- text_processing/src/string_parsing.rs
+++ text_processing/src/string_parsing.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use unicode_segmentation::UnicodeSegmentation;
 
 /// Collects all the Unicode graphemes in a string.
@@ -21,4 +22,37 @@ use unicode_segmentation::UnicodeSegmentation;
 /// ```
 pub fn collect_unicode_graphemes(text: &str) -> Vec<&str> {
     UnicodeSegmentation::graphemes(text, true).collect::<Vec<&str>>()
+}
+
+/// A struct that holds a full name.
+///
+/// # Example
+///
+/// ```
+/// use std::str::FromStr;
+/// use text_processing::FullName;
+///
+/// let full_name = FullName::from_str("John Doe").unwrap();
+/// assert_eq!(full_name.first_name, "John");
+/// assert_eq!(full_name.last_name, "Doe");
+/// ```
+#[derive(Debug, PartialEq)]
+pub struct FullName {
+    pub first_name: String,
+    pub last_name: String,
+}
+impl FromStr for FullName {
+    type Err = std::string::String;
+
+    // Get the first and last names from a full name.
+    fn from_str(full_name: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = full_name.split_whitespace().collect();
+        if parts.len() != 2 {
+            return Err("Full name must have two parts".to_string());
+        }
+        Ok(FullName {
+            first_name: parts[0].to_string(),
+            last_name: parts[1].to_string(),
+        })
+    }
 }
--- text_processing/tests/tests_string_parsing.rs
+++ text_processing/tests/tests_string_parsing.rs
@@ -1,7 +1,9 @@
-use text_processing::collect_unicode_graphemes;
+use std::str::FromStr;
+use text_processing::{collect_unicode_graphemes, FullName};
 
 #[cfg(test)]
 mod tests_string_parsing {
+
     use super::*;
 
     #[test]
@@ -10,5 +12,13 @@ mod tests_string_parsing {
         let graphemes = collect_unicode_graphemes(text);
         println!("{:?}", graphemes);
         assert_eq!(graphemes, vec!["a̐", "é", "ö̲", "\r\n"]);
+    }
+
+    #[test]
+    fn test_full_name_from_str() {
+        let full_name = FullName::from_str("John Doe").unwrap();
+        println!("{:?}", full_name);
+        assert_eq!(full_name.first_name, "John");
+        assert_eq!(full_name.last_name, "Doe");
     }
 }
