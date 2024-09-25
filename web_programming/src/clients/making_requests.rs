e6b1676ab25319b2822c368190eef6d321f8ddd2
eduairet <hola@eduairet.com>
2024-09-24 21:06:28 -0600

    Async get request

--- web_programming/src/clients/making_requests.rs
+++ web_programming/src/clients/making_requests.rs
@@ -31,3 +31,34 @@ pub fn make_get_request(endpoint: &str) -> (StatusCode, HeaderMap, String) {
     let headers = res.headers().clone();
     (status, headers, body)
 }
+
+/// Makes a GET request to the given endpoint and returns the status code, headers, and body.
+///
+/// # Arguments
+///
+/// * `endpoint` - A string slice that holds the URL of the endpoint.
+///
+/// # Returns
+///
+/// A tuple containing the status code, headers, and body of the response.
+///
+/// # Example
+///
+/// ```
+/// use web_programming::make_get_request_async;
+///
+/// #[tokio::main]
+/// async fn main() {
+///    let endpoint = "https://www.rust-lang.org/";
+///    let (status, headers, body) = make_get_request_async(endpoint).await;
+///    assert_eq!(status, 200);
+///    assert!(headers.contains_key("date"));
+///    assert!(body.contains("Rust"));
+/// }
+pub async fn make_get_request_async(endpoint: &str) -> (StatusCode, HeaderMap, String) {
+    let res = reqwest::get(endpoint).await.unwrap();
+    let status = res.status();
+    let headers = res.headers().clone();
+    let body = res.text().await.unwrap();
+    (status, headers, body)
+}
--- web_programming/tests/tests_clients.rs
+++ web_programming/tests/tests_clients.rs
@@ -1,4 +1,4 @@
-use web_programming::make_get_request;
+use web_programming::{make_get_request, make_get_request_async};
 
 #[cfg(test)]
 mod tests_clients {
@@ -8,6 +8,18 @@ mod tests_clients {
     fn test_make_get_request() {
         let endpoint = "https://www.rust-lang.org/";
         let (status, headers, body) = make_get_request(endpoint);
+        println!("status: {}", status);
+        println!("headers: {:?}", headers);
+        println!("body: {}", body.chars().take(100).collect::<String>());
+        assert_eq!(status, 200);
+        assert!(headers.contains_key("date"));
+        assert!(body.contains("Rust"));
+    }
+
+    #[tokio::test]
+    async fn test_make_get_request_async() {
+        let endpoint = "https://www.rust-lang.org/";
+        let (status, headers, body) = make_get_request_async(endpoint).await;
         println!("status: {}", status);
         println!("headers: {:?}", headers);
         println!("body: {}", body.chars().take(100).collect::<String>());
